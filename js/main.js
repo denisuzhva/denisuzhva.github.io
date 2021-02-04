40a54cf198869e9b6d7cd83c1c0ecd7df9bfabf1
denisuzhva <denis.uzhva@yahoo.com>
2021-02-04 19:06:14 +0300

    finished layout; full mobile support; refactoring

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+function dropDownOnMobile() {
+	var x = document.getElementById("theNavbar");
+		if (x.className === "navbar") {
+		x.className += " expanded-navbar";
+	} else {
+		x.className = "navbar";
+	}
+} 
